6d76b8035b43b5999a6abfb354fb5ab152aa9dde
Mihkel Tiganik <mihkel.tiganik@ee.abb.com>
2019-03-07 16:16:08 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cognex.DataMan.SDK;
+using Cognex.DataMan.SDK.Discovery;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			//Cognex.DataMan.SDK.ConnectionState connectionState = new ConnectionState();
+			//Cognex.DataMan.SDK.ReadStringArrivedEventArgs readStringArrivedEventArgs = new ReadStringArrivedEventArgs();
+
+			SerSystemDiscoverer.SystemInfo systemInfo;
+			SerSystemDiscoverer discoverer = new SerSystemDiscoverer();
+
+			discoverer.SystemDiscovered += new SerSystemDiscoverer.SystemDiscoveredHandler((si) => {
+				Console.WriteLine("Found device: {0} \n {1} \n {2} \n {3} \n {4}", si.Name, si.SerialNumber, si.Baudrate, si.Type, si.PortName);
+				systemInfo = si;
+
+			});
+			discoverer.Discover();
+
+			//if(systemInfo != null)
+			//{
+
+			//	Cognex.DataMan.SDK.SerSystemConnector myConn = new SerSystemConnector("COM5", );
+			//}
+
+			//var res = readStringArrivedEventArgs.ReadString;
+			Console.ReadLine();
+		}
+	}
+}
